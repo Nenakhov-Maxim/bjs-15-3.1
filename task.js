1b33ee4e61070979d3495ac07eaceb06ceade846
Nenakhov.Max@yandex.ru <Nenakhov.Max@yandex.ru>
2020-10-24 14:23:45 +0500

    Задание 2

--- task.js
+++ task.js
@@ -19,3 +19,45 @@ function validateCount(num) {
     }
 
 }
+
+// Задание 2
+
+class Triangle {
+    constructor(a, b, c) {
+        if (((a + b) < c) || ((a + c) < b) || ((b + c) < a)) {
+            throw new Error('Треугольник с такими сторонами не существует');
+        } else {
+            this.a = a;
+            this.b = b;
+            this.c = c;
+        }
+
+    }
+    getPerimeter() {
+        let perimeter = this.a + this.b + this.c;
+        return Number.parseFloat(perimeter);
+    }
+    getArea() {
+        let p = this.getPerimeter() / 2
+        let S = Math.sqrt(p * (p - this.a) * (p - this.b) * (p - this.c));
+        return Number.parseFloat(S.toFixed(3));
+    }
+}
+
+function getTriangle(a, b, c) {
+    debugger;
+    try {
+        let triangle = new Triangle(a, b, c);
+        return triangle;
+    } catch {
+        let triangleError = {
+            getArea() {
+                return "Ошибка! Треугольник не существует";
+            },
+            getPerimeter() {
+                return "Ошибка! Треугольник не существует";
+            }
+        }
+        return triangleError;
+    }
+}
